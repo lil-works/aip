04e2f28226f48f7263d6185e9c48a1ccf5eb2ef5
lil-works <lilian@lil-works.com>
2016-12-26 14:36:24 +0100

    some more feature

--- src/AppBundle/Form/PdfType.php
+++ src/AppBundle/Form/PdfType.php
@@ -7,14 +7,28 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
+use Symfony\Component\Validator\Constraints\File;
 
 class PdfType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('file', FileType::class, array('label' => 'PDF file' ,  'attr' => array('class'=>'btn btn-default btn-file')))
+            ->add('file', FileType::class, array(
+                'label' => 'PDF file' ,
+                'attr' => array('class'=>'btn btn-default btn-file'),
+                    'constraints' => [
+                        new File([
+                            'maxSize' => '80M',
+                            'mimeTypes' => [
+                                'application/pdf',
+                                'application/x-pdf',
+                            ],
+                            'mimeTypesMessage' => 'Please upload a valid PDF',
+                        ])
+                    ]
+                )
+            )
             ->add('compress', SubmitType::class , array('attr'=>array('class'=>'btn btn-primary')))
         ;
     }
